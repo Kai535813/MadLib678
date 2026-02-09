3f381d06e5fe2e56194cb1daf35d804d8f6efbb4
Kai535813 <9671235@graniteschools.org>
2026-02-09 16:56:39 -0700

    Add files via upload

--- src/MadLibP.py
+++ src/MadLibP.py
@@ -1,26 +1,24 @@
-Start = input("Would you like to create a Mad Lib story? yes/no: ")
-while Start == "yes":
-    Topic = input("What kind of topic would you like? High School Crush/fever dream/Colonoscopy/Political Speech: ")
-    if Topic == "High School Crush":
-        livingThing = input("Type a living thing: ")
-        maleName = input("Type a male name: ")
-        placeName = input("Type a name of a place: ")
-        femaleName = input("Type a female name: ")
-        ethnicity = input("Type a ethnicity: ")
-        charmingCharacteristic = input("Type a charming characteristic: ")
-        schoolClass = input("Type a school class: ")
-        actionOfLooking = input("Type a different way of saying 'looking': ")
-        className2 = input("Type a second class name: ")
-        liquid = input("Type an inedible liquid: ")
-        bodyPart = input("Type a body part from the shoulder to the fingers: ")
-        food = input("Type a food: ")
-        instrument = input("Type a musical instrument: ")
-        placeName2 = input("Type a second name of a place: ")
-        name2 = input("Type a name: ")
-        name3 = input("Type a second name: ")
-        name1 = input("Type a third name: ")
+Topic = input("What kind of topic would you like? High School Crush/2/3/4: ")
+if Topic == "High School Crush":
+    livingThing = input("Type a living thing: ")
+    maleName = input("Type a male name: ")
+    placeName = input("Type a name of a place: ")
+    femaleName = input("Type a female name: ")
+    ethnicity = input("Type a ethnicity: ")
+    charmingCharacteristic = input("Type a charming characteristic: ")
+    schoolClass = input("Type a school class: ")
+    actionOfLooking = input("Type a different way of saying 'looking': ")
+    className2 = input("Type a second class name: ")
+    liquid = input("Type an inedible liquid: ")
+    bodyPart = input("Type a body part from the shoulder to the fingers: ")
+    food = input("Type a food: ")
+    instrument = input("Type a musical instrument: ")
+    placeName2 = input("Type a second name of a place: ")
+    name2 = input("Type a name: ")
+    name3 = input("Type a second name: ")
+    name1 = input("Type a third name: ")
 
-        print("There once was a " + livingThing +" named " + maleName + ". " + maleName + " lived in " + placeName + 
+    print("There once was a " + livingThing +" named " + maleName + ". " + maleName + " lived in " + placeName + 
     " with his mom, dad, and younger sister. " + maleName + " had just got up and gotten ready for school. "
     + maleName + " loved to go to school because he had feelings for this girl " + femaleName + ", but he "
     " was also pretty nervous because " + maleName + " was performing in the " + placeName + " annual talent"
@@ -38,73 +36,78 @@ while Start == "yes":
     " he saw his crush " + femaleName + " in the crowd. He started playing his " + instrument + " and after the crowd"
     " broke into applause and " + femaleName + " ran up to him to confess her love and they lived happily ever after"
     " in " + placeName2 + ".")
-    if Topic == "fever dream":
-        positiveCharactaristic1 = input("Type a positive characteristic: ")
-        positiveCharactaristic2 = input("Type a second positive characteristic: ")
-        adjective3 = input("Type an adjective: ")
-        noun4 = input("Type a noun: ")
-        name5 = input("Type a name: ")
-        adjective6 = input("Type a second adjective: ")
-        adjective7 = input("Type a third adjective: ")
-        famousMaleActor8 = input("Type a famous male actor: ")
-        famousCatchphrase9 = input("Type a famous catchphrase: ")
-        vehicle10 = input("Type a type of vehicle: ")
-        foreignDangerousCountry11 = input("Type a foreign dangerous country: ")
-        superpower12 = input("Type a superpower: ")
-        computerProgrammingConcept13 = input("Type a computer programming concept: ")
-        pairOfLimbs14 = input("Type a pair of limbs (e.g. arms, legs): ")
-        adjective15 = input("Type an adjective: ")
-        print("There once was a " + positiveCharactaristic1 + " class called computer programming 2 at skyline high school. " 
-              "This class had a " + positiveCharactaristic2 + " teacher named Mr. Kapptee. One " + adjective3 + " November there "
-              "was a " + noun4 + " named " + name5 + " who took Mr. Kapptee's computer class with all the rest of the " + noun4 +
-             "s. Then suddenly a " + adjective6 + " "+ adjective7 + " guy named " + famousMaleActor8 + " broke through the glass "
-             "door in the back corner of the room and said his signature catchphrase \"" + famousCatchphrase9 + "\" and took " + name5 +
-             " and Mr. Kapptee by their arms and stuffed him in his "+ vehicle10 +" where he took their passports and took "
-             "them to "+ foreignDangerousCountry11 +" where "+ famousMaleActor8 +" thought they would never be able to escape from. "
-             "However he didn’t know that Mr. Kapptee had the powers of "+ superpower12 +" as well as flight and used his "+ superpower12 
-             +" to absolutely destroy "+ famousMaleActor8 +" after which he flew "+ name5 +" back to school with the other "+ noun4 +
-             "s where they continued to learn about "+ computerProgrammingConcept13 +" after which "+ name5+" got up on his "+ pairOfLimbs14 +
-             " and asked in a "+ adjective15 +" voice, 'how the heck do you have superpowers?' Just as Mr. Kapptee was about to explain," 
-             +name5+ " felt weird and then he woke up and figured out it was all just a fever dream he had after eating too much pizza the night before.")
-    if Topic == "Colonoscopy":
-        Input1 = input("Type a male name: ")
-        Input2 = input("Type a reason : ")
-        Input3 = input("Type a: ")
-        Input4 = input("Type a: ")
-        Input5 = input("Type a: ")
-        Input6 = input("Type a: ")
-        Input7 = input("Type a: ")
-        Input8 = input("Type a: ")
-        Input9 = input("Type a: ")
-        Input10 = input("Type a: ")
-        Input11 = input("Type a: ")
-        Input12 = input("Type a: ")
-        Input13 = input("Type a: ")
-        Input14 = input("Type a: ")
-        Input15 = input("Type a: ")
-        print(Input1 + " is finally read for his Colonoscopy after a whole night of defecating loose liquid stool" + Input1 + " was very grouchy when he got to his appoitment because " 
-             + Input2  + ". The real reason that" + Input1 + "decided to get a Colonoscopy on this day, was so that he could miss his daughter's " + Input3 + ".")
-    if Topic == "Political Speech":
-        Input1 = input("Choose a funny occupation: ")
-        Input2 = input("Type of drug: ")
-        Input3 = input("A type of company: ")
-        Input4 = input("Something funny: ")
-        Input5 = input("Something that you own: ")
-        Input6 = input("A large positive integer number: ")
-        Input7 = input("An even larger positive integer number: ")
-        Input8 = input("One more even larger positive integer number: ")
-        Input9 = input("Type a direction: ")
-        Input10 = input("Something cute and friendly: ")
-        Input11 = input("Any thing: ")
-        Input12 = input("Another different thing: ")
-        Input13 = input("Another funny occupation: ")
-        Input14 = input("A large commercial website: ")
-        Input15 = input("A type of service: ")
-        print(f"""This Mad Lib is not meant to endores the speaker, " \
-        "it is rather that this person is relavent and the speech was very convinient to use.""")
-        Understand = input("Move forward? Type y: ")
-        if Understand == "y":
-            print(f"""In addition, I'm doing what no {Input1} of either party has ever done. 
+elif Topic == "2":
+    Input1 = input("Type a: ")
+    Input2 = input("Type a: ")
+    Input3 = input("Type a: ")
+    Input4 = input("Type a: ")
+    Input5 = input("Type a: ")
+    Input6 = input("Type a: ")
+    Input7 = input("Type a: ")
+    Input8 = input("Type a: ")
+    Input9 = input("Type a: ")
+    Input10 = input("Type a: ")
+    Input11 = input("Type a: ")
+    Input12 = input("Type a: ")
+    Input13 = input("Type a: ")
+    Input14 = input("Type a: ")
+    Input15 = input("Type a: ")
+    print("Mad Libs Testing Format: " + Input1 + Input2 + Input3)
+elif Topic == "3":
+    Input1 = input("Type a male name: ")
+    Input2 = input("Things that would be suprising to find: ")
+    Input3 = input("Adjective: ")
+    Input4 = input("Object: ")
+    Input5 = input("Funny object: ")
+    Input6 = input("Another funny object: ")
+    Input7 = input(f"""A singlar thing you could "technically" consume: """)
+    Input8 = input("A toxic liquid: ")
+    Input9 = input("A very strong drink: ")
+    Input10 = input("A drink very high in caffine: ")
+    Input11 = input("A very young age: ")
+    Input12 = input("A very old age: ")
+    Input13 = input("A Celebrity name: ")
+    Input14 = input("An important personal document: ")
+    Input15 = input("Another important personal document: ")
+    print(f"""{Input1} was finally able to get a colonoscopy since his insurance company finally pays for it.
+              The purpose of the colonoscopy was to check for {Input2} in his colon. He was very nervous about the precedure
+               because he had heard that a friend of his found a very {Input3} {Input4} up his colon. {Input1} was really 
+                hoping that the Gastroenterologist would only find a {Input5} during his colonoscopy. {Input1} could not think 
+                of any reasons for why anything else like a {Input6} might be found, but he prayed just in case. {Input1} just
+                wished that he would never have to do this again, but he knew that it would be inevitable. The {Input7} 
+                diet and the {Input8} formula to purge his gut did not allow him a good night's rest. But not all was bad
+                there were some perks of coming to this specific clinic to get his colonoscopy done, this place
+                would allow {Input1} to choose out any anaesthetic he wanted. For today's procedure he would choose
+                {Input9} to knock him out. To wake him back up, they would pump {Input10} back into his viens.
+                This clinic was really at the forefront of health care practices as they would operate on anyone
+                 from the ages of {Input11} to {Input12}. Some Gastroenterologist voice concerns of operating on
+                newborns, but the {Input13} Clinic assures {Input1} that all is undercontrol. The {Input13} Clinic
+                 also takes patient privacy very seriously, so they never keep patient records, and never ask for any
+                  personal information. The only things they ask for are {Input14} and your {Input15}...
+                 2 days later, {Input1} was informed that he was happily {Input4} free.""")
+    
+elif Topic == "4":
+    Input1 = input("Choose a funny occupation: ")
+    Input2 = input("Type of drug: ")
+    Input3 = input("A type of company: ")
+    Input4 = input("Something funny: ")
+    Input5 = input("Something that you own: ")
+    Input6 = input("A large positive integer number: ")
+    Input7 = input("An even larger positive integer number: ")
+    Input8 = input("One more even larger positive integer number: ")
+    Input9 = input("Type a direction: ")
+    Input10 = input("Something cute and friendly: ")
+    Input11 = input("Any thing: ")
+    Input12 = input("Another different thing: ")
+    Input13 = input("Another funny occupation: ")
+    Input14 = input("A large commercial website: ")
+    Input15 = input("A type of service: ")
+    print(f"""This Mad Lib is not meant to endorse the speaker,  
+        it is rather that this person is relavent and the speech 
+          was very convinient to use.""")
+    Understand = input("Move forward? Type y: ")
+    if Understand == "y":
+        print(f"""In addition, I'm doing what no {Input1} of either party has ever done. 
                    Standing up to the special interests to dramatically reduce the price of {Input2} drugs. 
                    I negotiated directly with the {Input3}
                  and {Input4}, which were taken advantage of our {Input5} for many decades  
@@ -122,10 +125,5 @@ while Start == "yes":
                   And these big price cuts will greatly reduce the cost of {Input15}.""")
 
     
-    print("Why end now?! ")
-while Start != "yes":
-    print(f"""You could have had so much fun, but you choose not to. Bye Bye, 
-          but I mean if you choose to leave it's obvious you do not value Util, 
-          therefore you will loose your next ballot. And judge, 
-          this is why you should vote for the affirmative. """)
-    break
+
+    
